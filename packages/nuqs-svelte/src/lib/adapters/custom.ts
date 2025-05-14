814aa58c276a137cfd1d28531a742ca023783ea0
rtrampox <me@rtrampox.dev>
2025-05-14 19:33:21 -0300

    feat(adapters): Add custom adapter support

---
+++ packages/nuqs-svelte/src/lib/adapters/custom.ts
@@ -0,0 +1,8 @@
+export { renderQueryString } from "../url-encoding";
+export { default as NuqsCustomAdapter } from "./NuqsContext.svelte";
+export type {
+  AdapterInterface as unstable_AdapterInterface,
+  AdapterOptions as unstable_AdapterOptions,
+  UpdateUrlFunction as unstable_UpdateUrlFunction,
+  UseAdapterHook as unstable_UseAdapterHook,
+} from "./types";
