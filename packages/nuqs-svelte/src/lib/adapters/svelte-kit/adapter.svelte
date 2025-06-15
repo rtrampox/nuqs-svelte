58f8cd31ee289d7065460f6bdd1dc61a27ddd26a
rtrampox <me@rtrampox.dev>
2025-06-15 13:34:27 -0300

    feat(adapters): add adapter level options

--- packages/nuqs-svelte/src/lib/adapters/svelte-kit/adapter.svelte
+++ packages/nuqs-svelte/src/lib/adapters/svelte-kit/adapter.svelte
@@ -3,21 +3,26 @@
   import { goto, pushState, replaceState } from "$app/navigation";
   import { page } from "$app/state";
   import type { Snippet } from "svelte";
-  import type { AdapterInterface } from "../types";
+  import type { AdapterInterface, AdapterOptions } from "../types";
   import NuqsContext from "../NuqsContext.svelte";
   import { renderQueryString } from "$lib/url-encoding";
   import { SvelteURL } from "svelte/reactivity";
 
   type Props = {
     children?: Snippet;
+    /**
+     * Options defined at adapter level take precedence over options setted at hook (`useQueryState`) level.
+     */
+    options?: AdapterOptions;
   };
 
-  let { children }: Props = $props();
+  let { children, options }: Props = $props();
 
   // Using page.url directly will update the URL without invalidating the loaders dependent on it.
   let url = $derived(new SvelteURL(page.url));
 
   const adapter: AdapterInterface = {
+    options,
     updateUrl: (search, options) => {
       const { history, scroll, shallow } = options;
 
